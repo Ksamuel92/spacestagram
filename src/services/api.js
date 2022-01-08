4b572acaf194bb31f8cce4e5f25f35463645d65a
Kyle Samuel <ksamuel92@gmail.com>
2022-01-08 18:51:51 -0500

    set api count query to 10

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,10 @@
-import useQuery from "react-query";
+import { useQuery } from "react-query";
 export const useGetPhotosQuery = () => {
   const { isLoading, error, isError, data, isFetching, isSuccess } = useQuery(
     "getPhotos",
     async () => {
       const response = await fetch(
-        "https://api.nasa.gov/planetary/apod?api_key=2FVxEhAfBgmNsZinZFXC3LF5GXNIx6NAKBi4JNO3"
+        "https://api.nasa.gov/planetary/apod?api_key=2FVxEhAfBgmNsZinZFXC3LF5GXNIx6NAKBi4JNO3&count=10"
       );
       if (!response.ok) {
         throw new Error("Something went wrong");
