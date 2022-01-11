dbf852f4c15603e4afb2e5c5ebed3bc250e640c3
Kyle Samuel <ksamuel92@gmail.com>
2022-01-11 14:13:58 -0500

    adjust API for infinitescroller

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,20 @@
-import { useQuery } from "react-query";
+import { useInfiniteQuery } from "react-query";
 export const useGetPhotosQuery = () => {
-  const { isLoading, error, isError, data, isFetching, isSuccess } = useQuery(
-    "getPhotos",
-    async () => {
-      const response = await fetch(
-        "https://api.nasa.gov/planetary/apod?api_key=2FVxEhAfBgmNsZinZFXC3LF5GXNIx6NAKBi4JNO3&count=10"
-      );
-      if (!response.ok) {
-        throw new Error("Something went wrong");
-      }
-
-      return response.json();
-    }
-  );
+  const {
+    isLoading,
+    error,
+    isError,
+    data,
+    isFetching,
+    isSuccess,
+    hasNextPage,
+    fetchNextPage,
+  } = useInfiniteQuery("photos", fetchPhotos, {
+    getNextPageParam: (lastPage, pages) => {
+      if (lastPage.nextPage < lastPage.totalPages) return lastPage.nextPage;
+      return undefined;
+    },
+  });
   return {
     data,
     error,
@@ -20,5 +22,15 @@ export const useGetPhotosQuery = () => {
     isError,
     isFetching,
     isSuccess,
+    hasNextPage,
+    fetchNextPage,
   };
+};
+
+const fetchPhotos = async ({ pageParam = 1 }) => {
+  const response = await fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=2FVxEhAfBgmNsZinZFXC3LF5GXNIx6NAKBi4JNO3&count=10`
+  );
+  const results = await response.json();
+  return { results, nextPage: pageParam + 1, totalPages: 100 };
 };
